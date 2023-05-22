4d2054be4518f4abd281102bd36491612f9a7c07
oguzcancakiner <118448922+oguzcancakiner@users.noreply.github.com>
2023-05-22 10:12:14 +0300

    Add project files.

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+        }
+    }
+}
---
+++ Donguler/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım 1";
+            string kurs2 = "Yazılım 2";
+            string kurs3 = "Java";
+
+            // array - dizi
+
+            string[] kurslar = new string[] { "Yazılım 1", "Yazılım 2", "Java" };
+
+            for (int i = 1; i <= 10; i=i+2)
+            {
+                Console.WriteLine(i);
+
+            }
+        }
+    }
+}
+
