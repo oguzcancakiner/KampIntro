1b40bb2be9a8c519f0040b4d4219814111855bf4
oguzcancakiner <118448922+oguzcancakiner@users.noreply.github.com>
2023-05-23 14:34:39 +0300

    23.05

---
+++ OOP3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager  ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(tasitKrediManager, new List<ILoggerService> {new DatabaseLoggerService() });
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager,  };
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
